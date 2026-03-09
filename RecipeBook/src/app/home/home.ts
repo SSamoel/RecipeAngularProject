845bbfce5581e88c1e2b14621118515a21b09c74
SSamoel <ssamoel@outlook.com>
2026-03-09 19:10:20 +0200

    turn code from impretive programming to  reactive programming

--- RecipeBook/src/app/home/home.ts
+++ RecipeBook/src/app/home/home.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RecipesSercices } from '../recipes/services/recipes-sercices';
 import { Recipe } from '../core/models/recipe.model';
 import { Blogservices } from '../shared/services/blogservices';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,64 +12,94 @@ import { Blogservices } from '../shared/services/blogservices';
 })
 export class Home implements OnInit {
 
-
+  // tags: string[] = [];
+  private tagSubject = new BehaviorSubject<string>('');
+  // mealTypes: string[] = [];
+  private mealTypeSubject = new BehaviorSubject<string>('');
+  // recipes: Recipe[] = [];
+  private recipesSubject = new BehaviorSubject<Recipe[]>([]);
+  selectedTag: string = '';
+  selectedMealType: string = '';
 
   tags: string[] = [];
   mealTypes: string[] = [];
-  selectedTag: string = '';
-  selectedMealType: string = '';
-  recipes: Recipe[] = [];
-  filteredRecipes: any[] = [];
   blogs: any[] = [];
+  filteredRecipes: any[] = [];
   limit = 6;
   skip = 0;
 
-  constructor(private recipesService: RecipesSercices , private blogService : Blogservices) { }
+  constructor(private recipesService: RecipesSercices, private blogService: Blogservices) { }
 
   ngOnInit(): void {
-    this.recipesService.getRecipes(this.limit, this.skip).subscribe((data) => {
-      this.recipes = data.recipes;
-      this.filteredRecipes = this.recipes;
+    // this.recipesService.getRecipes(this.limit, this.skip).subscribe((data) => {
+    //   this.recipes = data.recipes;
+    //   this.filteredRecipes = this.recipes;
 
-      this.mealTypes = [...new Set(
-        this.recipes.flatMap((r: any) => r.mealType)
-      )]
+    //   this.mealTypes = [...new Set(
+    //     this.recipes.flatMap((r: any) => r.mealType)
+    //   )]
+    // });
+
+    // this.recipesService.getRecipesTags().subscribe({
+    //   next: (res) => {
+    //     this.tags = res;
+    //   },
+    //   error: (err) => {
+    //     console.error(err);
+    //   }
+    // });
+
+    this.recipesService.getRecipes(this.limit, this.skip).subscribe((data) => {
+      this.recipesSubject.next(data.recipes);
+      this.mealTypes = [...new Set(data.recipes.flatMap((r: any) => r.mealType))];
     });
 
     this.recipesService.getRecipesTags().subscribe({
       next: (res) => {
         this.tags = res;
       },
-      error: (err) => {
-        console.error(err);
-      }
+      error: (err) => console.error(err)
     });
 
-    this.blogService.getBlogs().subscribe((data)=>{
-      this.blogs = data.blogs.slice(0,5);
+    combineLatest({
+      tag: this.tagSubject,
+      mealType: this.mealTypeSubject,
+      allRecipes: this.recipesSubject
+    }).pipe(
+      map(({ tag, mealType, allRecipes }) => {
+        return allRecipes.filter(recipe => {
+          const tagMatch = !tag || recipe.tags.includes(tag);
+          const mealTypeMatch = !mealType || recipe.mealType.includes(mealType);
+          return tagMatch && mealTypeMatch;
+        });
+      })
+    ).subscribe(filteredList => {
+      this.filteredRecipes = filteredList;
+    });
+    this.blogService.getBlogs().subscribe((data) => {
+      this.blogs = data.blogs.slice(0, 5);
     })
-
   }
 
-  applyFilters() {
-    this.filteredRecipes = this.recipes.filter(recipe => {
-      const tagMatch = !this.selectedTag || recipe.tags.includes(this.selectedTag);
-      const mealTypeMatch = !this.selectedMealType || recipe.mealType.includes(this.selectedMealType);
-      return tagMatch && mealTypeMatch;
-    })
-  }
+  // applyFilters() {
+  //   this.filteredRecipes = this.recipes.filter(recipe => {
+  //     const tagMatch = !this.selectedTag || recipe.tags.includes(this.selectedTag);
+  //     const mealTypeMatch = !this.selectedMealType || recipe.mealType.includes(this.selectedMealType);
+  //     return tagMatch && mealTypeMatch;
+  //   })
+  // }
 
   onTagChange(tag: string) {
     this.selectedTag = tag;
-    this.applyFilters();
+    this.tagSubject.next(tag);
   }
 
   onMealTypeChange(mealType: string) {
     this.selectedMealType = mealType;
-    this.applyFilters();
+    this.mealTypeSubject.next(mealType);
   }
 
-  get MainBlog(){
+  get MainBlog() {
     return this.blogs[0];
   }
 }
